ed6dbd653bc74c4028200e8838b1a8708fd02999
Polina Fedorova <polina.fedorova@devexpress.com>
2023-01-26 18:03:54 +0400

    update target to 6

--- VB/My Project/AssemblyInfo.vb
+++ VB/My Project/AssemblyInfo.vb
@@ -4,18 +4,6 @@ Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 Imports System.Windows
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-<Assembly: AssemblyTitle("Expander")> 
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("DevExpress")>
-<Assembly: AssemblyProduct("AutoCorrectEvent")>
-<Assembly: AssemblyCopyright("Copyright © DevExpress 2010")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
@@ -36,17 +24,3 @@ Imports System.Windows
     ' or application resource dictionaries)
     '(used if a resource is not found in the page, 
     ' app, or any theme specific resource dictionaries)
-
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
